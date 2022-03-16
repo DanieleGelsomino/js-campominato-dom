7647672649680c08d71de7fab58e50e967faa44f
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-16 14:00:22 +0100

    add corrections init

--- js/script.js
+++ js/script.js
@@ -18,27 +18,27 @@ buttonMedium.addEventListener("click", () =>
 buttonHard.addEventListener("click", () => createElementsInGrid(49, "hard"));
 
 function createElementsInGrid(totalCells, levelClass) {
-  // 1. recupero la griglia con l'id
+  // recupero la griglia con l'id
   const grid = document.getElementById("grid");
   // resetto il contenuto della griglia
   grid.innerHTML = "";
 
-  // 2. creo totalCells div all'interno della griglia
+  // creo totalCells div all'interno della griglia
   for (let i = 0; i < totalCells; i++) {
-    //      2a: creo l'elemento
+    // creo l'elemento
     const cell = document.createElement("div");
-    //      2b: aggiungo eventuali classi css per dargli uno stile
+    // aggiungo eventuali classi css per dargli uno stile
     // cell.classList.add('cell');
     cell.className = "cell";
     cell.classList.add(levelClass);
-    //      2c: associamo il numero da 1 a 100 al testo contenuto nella cella
+    // associamo il numero da 1 a 100 al testo contenuto nella cella
     cell.innerText = i + 1;
-    //      2d: aggiungo l'elemento creato alla griglia
+    // aggiungo l'elemento creato alla griglia
     grid.appendChild(cell);
 
-    //      2e: aggiungo un listener sul click dell'elemento
-    // 2e-pt1: se non ha già lo sfondo azzurro lo metto
-    // 2e-pt2: se è già azzuro tolgo il colore dallo sfondo
+    // aggiungo un listener sul click dell'elemento
+    // se non ha già lo sfondo azzurro lo metto
+    // se è già azzuro tolgo il colore dallo sfondo
 
     cell.addEventListener("click", () => cell.classList.toggle("bg-skyblue"));
   }
--- js/steps.js
+++ js/steps.js
@@ -1,44 +1,28 @@
-console.log('JS OK!');
+console.log("JS OK!");
 
-/*
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-la griglia è composta da 100 elementi che visualizzano i numeri da 1 e 100 in ordine.
-
-
-Consigli del giorno: :party_wizard:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
-
-
-BONUS:
-L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
-
-*/
-
-
-// step by step
-// 1. recupero la griglia con l'id
-// 2. creo N (49, 81 o 100) div all'interno della griglia
-//      2a: creo l'elemento
-//      2b: aggiungo eventuali classi css per dargli uno stile
-//      2c: associamo il numero da 1 a N al testo contenuto nella cella
-//      2d: aggiungo l'elemento creato alla griglia
-//      2e: aggiungo un listener sul click dell'elemento
-//          2e-pt1: se non ha già lo sfondo azzurro lo metto
-//          2e-pt2: se è già azzuro tolgo il colore dallo sfondo
-
-
-// BONUS
-// 1. recupero gli elementi bottone
-// 2. aggiungo il listener al click ad ogni bottone
-    // al click chiamo una funzione che genera la griglia (vedi sopra)
-    // con 49, 81 o 100 celle in base al bottone cliccato.
-
-
-
+// Consegna:
+// Viene generata una griglia di gioco quadrata,
+// in cui ogni cella contiene un numero tra quelli compresi nel range 1-100.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati
+// - abbiamo calpestato una bomba
+// -la cella si colora di rosso e la partita termina,
+// altrimenti la cella cliccata si colora di azzurro
+// e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba
+// o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio,
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// BONUS:
+// 1- quando si clicca su una bomba e finisce la partita,
+// evitare che si possa cliccare su altre celle
+// 2- quando si clicca su una bomba e finisce la partita,
+// il software scopre tutte le bombe nascoste
+// 3- L'utente indica un livello di difficoltà in base al quale
+// viene generata una griglia di gioco quadrata,
+// in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
